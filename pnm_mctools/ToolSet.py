4cacdb26053f6fa5018ca23cb25dbc1dcffbd1ae
David <d.r.rieder@tue.nl>
2024-05-24 09:57:32 +0200

    update

--- pnm_mctools/ToolSet.py
+++ pnm_mctools/ToolSet.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy
 import scipy.sparse
 import inspect
-from NumericalDifferentiation import NumericalDifferentiation
+from . import NumericalDifferentiation
 
 
 def GetLineInfo():
