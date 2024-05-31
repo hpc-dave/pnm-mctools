dbbc59a5e6963585a1fb705496b2f08ebecea187
David <d.r.rieder@tue.nl>
2024-05-31 15:25:39 +0200

    removed adsorption from ini

--- pnm_mctools/__init__.py
+++ pnm_mctools/__init__.py
@@ -3,4 +3,3 @@ from .NumericalDifferentiation import NumericalDifferentiation      # noqa: F401
 from .calibrated_conductance import calibrated_conductance          # noqa: F401
 from .spheres_and_cylinders import spheres_and_cylinders            # noqa: F401
 from .Reactions import LinearReaction                               # noqa: F401
-import Adsorption                                                   # noqa: F401
