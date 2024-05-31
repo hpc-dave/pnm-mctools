11f3cc80b2c02549915c6bef90a1c5cbe222ff75
David <d.r.rieder@tue.nl>
2024-05-31 12:19:57 +0200

    added adsorption isotherms

--- pnm_mctools/Adsorption.py
+++ pnm_mctools/Adsorption.py
@@ -6,9 +6,72 @@ except ImportError:
     from NumericalDifferentiation import NumericalDifferentiation
 
 
+def Linear(c_f, K):
+    r"""
+    Linear adsorption, e.g. in the limit case of highly dilute species
+
+    Parameters
+    ----------
+    c_f
+        concentration in the fluid phase, usually in mol/m^3
+    K
+        equilibrium constant, usually in m^3/(m^2 mol)
+
+    Returns
+    -------
+    surface load
+    """
+    return c_f * K
+
+def Langmuir(c_f, K, y_max):
+    r""""
+    Single component Langmuir adsorption model
+
+    Parameters
+    ----------
+    c_f
+        concentration in the fluid phase, usually in mol/m^3
+    K
+        equilibrium constant, usually in m^3/mol
+    y_max
+        maximum surface load, usually in m^2/mol
+
+    Returns
+    -------
+    surface coverage
+    """
+    y = y_max * c_f*K/(1+c_f*K)
+    if isinstance(y, np.ndarray) and y.size != c_f.size:
+        raise ValueError('Apparently the dimensions are inconsistent and yield a broken field')
+    return y
+
+
+def Freundlich(c_f, K, n):
+    r"""
+    Freundlich isotherm
+
+    Parameters
+    ----------
+    c_f
+        concentration in the fluid phase, usually in mol/m^3
+    K
+        equilibrium constant, usually in m^(3n)/(mol^n m^2)
+    n
+        exponent of concentration
+
+    Returns
+    -------
+    surface load
+    """
+    y = K * c_f**(1./n)
+    if isinstance(y, np.ndarray) and y.size != c_f.size:
+        raise ValueError('Apparently the dimensions are inconsistent and yield a broken field')
+    return y
+
+
 def AdsorptionSingleComponent(c,
                               dilute: int, adsorbed: int,
-                              y_max, Vp, a_v, ftheta: Callable,
+                              Vp, a_v, y_func: Callable,
                               k_r: float = 1e6, type: str = 'Jacobian',
                               exclude=None, dc=1e-6):
     r"""
@@ -23,16 +86,14 @@ def AdsorptionSingleComponent(c,
         component ID of dilute species
     adsorbed: int
         component ID of adsorbed species
-    y_max
-        maximum surface load, usually in mol/m^2, can be array or single float
     Vp
         pore volume volume for scaling in m^3, ignored if set to 'None'
     a_v
         specific surface area in m^2/m^3
     ftheta: callable
-        function object which computes the equilibrium relative amount of occupied surface sites for a given
-        concentration of dilute and adsorbed species, signature of the function object needs to be:
-        (c_0: array_like, c_1: array_like): array_like
+        function object which computes the surface load for a given concentration of dilute and adsorbed species,
+        signature of the function object needs to be:
+            (c_0: array_like, c_1: array_like): array_like
         with c_0 as dilute species concentration, c_1 as adsorbed species concentration and returning the
         equilibrium value of the relative amount of occupied surface sites, the arrays are of size [Np, 1]
     k_r
@@ -56,6 +117,15 @@ def AdsorptionSingleComponent(c,
 
         r_v = -a_v * r_{ad} ~\mathrm{in mol/(m^3 s)}
 
+    For simplicity, the partial surface coverage was chosen, however for practical application this can be treated
+    as surface load, e.g. for Linear or Freundlich isotherms which do not exhibit a limit. The final surface load is
+    determined by the maximum surface load y_max by
+    .. math::
+
+        Y = y_{max} * \theta
+
+    So by appropriate manipulation of y_max, e.g. by y_max = 1, the surface load is directly computed by the isotherm
+
     Here a word of advice: The numerical differentiation is expensive, therefore it is not recommended to compute
     the Jacobian at each Newton-iteration. However, using only the Defect requires more iterations, so careful
     optimization is required by choosing between the two options!
@@ -69,10 +139,9 @@ def AdsorptionSingleComponent(c,
 
     def Defect(c):
         c_f = c[:, dilute].reshape((-1, 1))
-        c_ads = c[:, adsorbed].reshape((-1, 1))
-        theta = c_ads / y_max
-        theta_eq = ftheta(c_f, c_ads)
-        r_ads = k_r * y_max * (theta_eq - theta)
+        y_ads = c[:, adsorbed].reshape((-1, 1))
+        y_ads_eq = y_func(c_f, y_ads)
+        r_ads = k_r * (y_ads_eq - y_ads)
         r_f = -a_v * r_ads
         G = np.zeros((Np * Nc, 1), dtype=float)
         G[rows, :] = r_f
--- pnm_mctools/test_Adsorption.py
+++ pnm_mctools/test_Adsorption.py
@@ -5,12 +5,16 @@ import numpy as np
 import scipy
 from ToolSet import MulticomponentTools
 from Adsorption import AdsorptionSingleComponent
+from Adsorption import Linear, Langmuir, Freundlich
+
 
 def run(output: bool = True):
     success = True
     success &= run_Linear(output)
     success &= run_Langmuir(output)
+    success &= run_Freundlich(output)
     return success
+
 
 def run_Linear(output: bool = True):
     Nx = 10
@@ -30,14 +34,13 @@ def run_Linear(output: bool = True):
     ads = 0
     dil = 1
 
-    def ftheta(c_f, c_ads):
-        return c_f*0.5
+    def y_f(c_f, c_ads):
+        return Linear(c_f, K=0.5)
 
-    ymax = np.full((network.Np, 1), fill_value=99., dtype=float)
     a_V = np.full((network.Np, 1), fill_value=5., dtype=float)
 
     c = np.zeros((network.Np, Nc))
-    c[:, dil] = 1.
+    c[:, dil] = np.linspace(0.1, 1., c.shape[0])
     mt = MulticomponentTools(network=network, num_components=Nc)
 
     x = c.reshape((-1, 1))
@@ -53,8 +56,8 @@ def run_Linear(output: bool = True):
     def ComputeSystem(x, c_l, type):
         J_ads, G_ads = AdsorptionSingleComponent(c=c_l,
                                                  dilute=dil, adsorbed=ads,
-                                                 y_max=ymax, Vp=network['pore.volume'],
-                                                 a_v=a_V, ftheta=ftheta, exclude=2,
+                                                 Vp=network['pore.volume'], a_v=a_V,
+                                                 y_func=y_f, exclude=2,
                                                  type=type, dc=1e-6)
         G = ddt * (x - x_old) + G_ads
         if type == 'Defect':
@@ -85,8 +88,8 @@ def run_Linear(output: bool = True):
     err = np.sum(m - m_0)/np.sum(m_0)
     success &= err < 1e-12
 
-    theta_final = ftheta(c[:, dil], c[:, ads])
-    c_ads = ymax * theta_final
+    theta_final = y_f(c[:, dil], c[:, ads])
+    c_ads = theta_final
     err_ads = np.max(np.abs((c_ads-c[:, ads])/c_ads))
     success &= err_ads < 1e-5
     if output:
@@ -113,15 +116,14 @@ def run_Langmuir(output: bool = True):
     ads = 0
     dil = 1
 
-    def ftheta(c_f, c_ads):
-        Kcf = c_f
-        return Kcf / (1+Kcf)
-
     ymax = np.full((network.Np, 1), fill_value=95., dtype=float)
     a_V = np.full((network.Np, 1), fill_value=10., dtype=float)
 
+    def y_f(c_f, c_ads):
+        return Langmuir(c_f.reshape((-1, 1)), K=0.1, y_max=ymax)
+
     c = np.zeros((network.Np, Nc))
-    c[:, dil] = 1.
+    c[:, dil] = np.linspace(0.1, 1., c.shape[0])
     mt = MulticomponentTools(network=network, num_components=Nc)
 
     x = c.reshape((-1, 1))
@@ -137,8 +139,8 @@ def run_Langmuir(output: bool = True):
     def ComputeSystem(x, c_l, type):
         J_ads, G_ads = AdsorptionSingleComponent(c=c_l,
                                                  dilute=dil, adsorbed=ads,
-                                                 y_max=ymax, Vp=network['pore.volume'],
-                                                 a_v=a_V, ftheta=ftheta, exclude=2,
+                                                 Vp=network['pore.volume'], a_v=a_V,
+                                                 y_func=y_f, exclude=2,
                                                  type=type, dc=1e-6)
         G = ddt * (x - x_old) + G_ads
         if type == 'Defect':
@@ -169,11 +171,94 @@ def run_Langmuir(output: bool = True):
     err = np.sum(m - m_0)/np.sum(m_0)
     success &= err < 1e-12
 
-    theta_final = ftheta(c[:, dil], c[:, ads])
-    c_ads = ymax * theta_final
+    c_ads = y_f(c[:, dil], c[:, ads]).reshape((-1))
     err_ads = np.max(np.abs((c_ads-c[:, ads])/c_ads))
     success &= err_ads < 1e-5
     if output:
         print(f'{last_iter + 1} it [{G_norm:1.2e}]\
             mass-loss [{err:1.2e}] isotherm-error [{err_ads:1.2e}]')
     return success
+
+
+def run_Freundlich(output: bool = True):
+    Nx = 10
+    Ny = 1
+    Nz = 1
+    Nc = 3
+    spacing = 1./Nx
+
+    # get network
+    network = op.network.Cubic([Nx, Ny, Nz], spacing=spacing)
+
+    # add geometry
+    network.add_model_collection(op.models.collections.geometry.spheres_and_cylinders, domain='all')
+    network.regenerate_models()
+
+    # adsorption data
+    ads = 0
+    dil = 1
+
+    a_V = np.full((network.Np, 1), fill_value=10., dtype=float)
+
+    def y_f(c_f, c_ads):
+        return Freundlich(c_f, 0.1, 1.5)
+
+    c = np.zeros((network.Np, Nc))
+    c[:, dil] = np.linspace(0.1, 1., c.shape[0])
+    mt = MulticomponentTools(network=network, num_components=Nc)
+
+    x = c.reshape((-1, 1))
+    dx = np.zeros_like(x)
+
+    tol = 1e-12
+    max_iter = 100
+    ddt = mt.DDT(dt=1.)
+
+    success = True
+    x_old = x.copy()
+
+    def ComputeSystem(x, c_l, type):
+        J_ads, G_ads = AdsorptionSingleComponent(c=c_l,
+                                                 dilute=dil, adsorbed=ads,
+                                                 Vp=network['pore.volume'], a_v=a_V,
+                                                 y_func=y_f, exclude=2,
+                                                 type=type, dc=1e-6)
+        G = ddt * (x - x_old) + G_ads
+        if type == 'Defect':
+            return G
+        J = ddt + J_ads
+        return J, G
+
+    J, G = ComputeSystem(x, c, 'Jacobian')
+
+    m_0 = c.copy()
+    m_0[:, dil] *= network['pore.volume']
+    m_0[:, ads] *= network['pore.volume'] * a_V.reshape((-1))
+    for i in range(max_iter):
+        last_iter = i
+        dx = scipy.sparse.linalg.spsolve(J, -G).reshape(dx.shape)
+        x = x + dx
+        c = x.reshape(c.shape)
+        J, G = ComputeSystem(x, c, 'Jacobian')
+        G_norm = np.linalg.norm(np.abs(G), ord=2)
+        if G_norm < tol:
+            break
+    if last_iter == max_iter - 1:
+        print(f'WARNING: the maximum iterations ({max_iter}) were reached!')
+
+    m = c.copy()
+    m[:, dil] *= network['pore.volume']
+    m[:, ads] *= network['pore.volume'] * a_V.reshape((-1))
+    err = np.sum(m - m_0)/np.sum(m_0)
+    success &= err < 1e-12
+
+    c_ads = y_f(c[:, dil], c[:, ads])
+    err_ads = np.max(np.abs((c_ads-c[:, ads])/c_ads))
+    success &= err_ads < 1e-5
+    if output:
+        print(f'{last_iter + 1} it [{G_norm:1.2e}]\
+            mass-loss [{err:1.2e}] isotherm-error [{err_ads:1.2e}]')
+    return success
+
+
+run()
