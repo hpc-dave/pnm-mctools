06f5aec83ff866c2acc783f173c1ab1324c125bf
David <d.r.rieder@tue.nl>
2024-07-03 09:05:59 +0200

    removed testing from init

--- pnm_mctools/__init__.py
+++ pnm_mctools/__init__.py
@@ -1,6 +1,6 @@
 from .ToolSet import MulticomponentTools                            # noqa: F401
 from .NumericalDifferentiation import NumericalDifferentiation      # noqa: F401
 from .Reactions import LinearReaction                               # noqa: F401
-import testing                                                      # noqa: F401
+# import testing                                                      # noqa: F401
 # from .calibrated_conductance import calibrated_conductance          # noqa: F401
 # from .spheres_and_cylinders import spheres_and_cylinders            # noqa: F401
