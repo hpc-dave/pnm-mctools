02cfc61c2ac1b5d7143ccd46e54a15b67109d3dc
David <d.r.rieder@tue.nl>
2024-10-02 14:35:23 +0200

    fixed import bug

--- pnm_mctools/__init__.py
+++ pnm_mctools/__init__.py
@@ -1,3 +1,3 @@
-from .ToolSet import MulticomponentTools                            # noqa: F401
-from .NumericalDifferentiation import NumericalDifferentiation      # noqa: F401
-from .Reactions import LinearReaction                               # noqa: F401
+from .ToolSet import MulticomponentTools                                 # noqa: F401
+from .NumericalDifferentiation import conduct_numerical_differentiation  # noqa: F401
+from .Reactions import LinearReaction                                    # noqa: F401
