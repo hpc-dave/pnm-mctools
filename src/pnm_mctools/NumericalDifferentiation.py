ce1a31a52225e023f8569195f0d0b264075cbba0
David <d.r.rieder@tue.nl>
2025-09-20 11:46:22 +0200

    added documentation and integrated the function into the generic conduct_numerical_differentiation function
    Signed-off-by: David <d.r.rieder@tue.nl>

--- src/pnm_mctools/NumericalDifferentiation.py
+++ src/pnm_mctools/NumericalDifferentiation.py
@@ -228,88 +228,6 @@ def _apply_numerical_differentiation_locally_constrained(c: np.ndarray,
     return J, G_0
 
 
-def conduct_numerical_differentiation(c: np.ndarray, defect_func: Callable, dc: float = 1e-6, type: str = 'full',
-                                      exclude: int | list[int] | None = None, axis: int = None):
-    r"""
-    Conducts numerical differentiation
-
-    Parameters
-    ----------
-    c: array_like
-        array with scalar values, which serve as input for the defect function
-    defect_func: func
-        function which computes the defect with signature array_like(array_like, int)
-        where the second argument refers to the manipulated row
-    dc: float
-        base value for differentiation-interval
-    type: str
-        specifier for optimization of the process, currently supported arguments are
-        'full' and 'low_mem', for the allocation of an intermediated dense matrix
-        and sparse columns respectively. The option 'constrained' can be applied, if
-        the changes in defect are constrained to each pore, e.g. in the case of reaction
-    exclude: int|list[int]|None
-        component IDs for which the numerical differentiation shall not be conducted
-    axis: int
-        alternative to the `type` label for consistency with the mrm package
-
-    Returns
-    -------
-    tuple of numerically determined Jacobian and defect
-
-    Notes
-    -----
-    To save memory during the computation, the matrix entries are stored per column
-    in a sparse array and later stacked to form the full sparse array.
-    The in between conversion leads to a slight overhead compared with the approach
-    to directly add the components into an existing array, but decreases memory
-    demand significantly, especially for large matrices (>5000 rows)
-    """
-    if len(c.shape) > 2:
-        raise ('Input array has invalid dimension, only 2D arrays are allowed!')
-    elif len(c.shape) < 2:
-        raise ('Input array has to have a second dimension, indicating the number of components')
-    type_l = type
-    if axis is not None:
-        if axis == 0 and type is None:
-            type_l = 'full'
-        elif axis == 1:
-            type_l = 'constrained'
-        else:
-            raise ValueError('axis value as to be either 0 or 1!')
-
-    exclude = [] if exclude is None else exclude
-    exclude = [exclude] if isinstance(exclude, int) else exclude
-    if not isinstance(exclude, list):
-        raise TypeError('the provided exclude list is not an integer or a list of integers')
-
-    num_param = len(signature(defect_func)._parameters)
-    if num_param == 0:
-        raise ValueError('The provided defect function does not take any arguments!')
-    elif num_param > 2:
-        raise ValueError('Number of arguments for defect function is larger than 2!')
-
-    dc_arr = _compute_dc(c.reshape((-1, 1)), dc)
-
-    if type_l == 'constrained':
-        J, G_0 = _apply_numerical_differentiation_locally_constrained(c=c,
-                                                                      defect_func=defect_func,
-                                                                      dc=dc_arr,
-                                                                      exclude=exclude)
-    elif type_l == 'full':
-        J, G_0 = _apply_numerical_differentiation_full(c=c,
-                                                       defect_func=defect_func,
-                                                       dc=dc_arr,
-                                                       exclude=exclude)
-    elif type_l == 'low_mem':
-        J, G_0 = _apply_numerical_differentiation_lowmem(c=c,
-                                                         defect_func=defect_func,
-                                                         dc=dc_arr,
-                                                         exclude=exclude)
-    else:
-        raise (f'Unknown type: {type}')
-    return scipy.sparse.csr_matrix(J), G_0.reshape((-1, 1))
-
-
 def _apply_numerical_differentiation_exploit_sparsity(
         network,
         c: np.ndarray,
@@ -317,6 +235,36 @@ def _apply_numerical_differentiation_exploit_sparsity(
         dc: float,
         stencil_size: int = 1,
         dtype=float):
+    r"""
+    Conducts numerical differentiation, exploiting the sparsity structure of the network
+    Parameters
+    ----------
+    network: OpenPNM Network object
+        The network object is used to determine the sparsity structure of the Jacobian.
+    c: array_like
+        array with scalar values, which serve as input for the defect function
+    defect_func: Callable
+        function which computes the defect with signature array_like(array_like)
+    dc: float
+        base value for differentiation interval
+    stencil_size: int
+        number of additional adacent pores to include in the sparsity structure
+    dtype: data-type
+        desired data-type of the scalars in the Jacobian
+
+    Returns
+    -------
+    tuple of numerically determined Jacobian and defect
+
+    Notes
+    -----
+    Here, we exploit the sparsity structure of the network to speed up the numerical
+    differentiation. The basic assumption is that the influence of a perturbance in
+    one pore is limited to its direct neighbours and potentially some additional
+    pores, depending on the stencil size. This way, we can determine the influence
+    of multiple perturbances in one run of the defect function, significantly reducing
+    the computational cost.
+    """
 
     # analyse the sparsity structure
     adj = network.create_adjacency_matrix(weights=np.ones_like(network['throat.conns'], dtype=bool), fmt='csr')
@@ -366,6 +314,104 @@ def _apply_numerical_differentiation_exploit_sparsity(
             J += scipy.sparse.coo_matrix((values, (rows, cols)), shape=shape_jac, dtype=dtype)
     J = scipy.sparse.csr_matrix(J)
     return J, G0
+
+
+def conduct_numerical_differentiation(c: np.ndarray, defect_func: Callable, dc: float = 1e-6, type: str = 'full',
+                                      exclude: int | list[int] | None = None, axis: int = None,
+                                      network=None, stencil_size: int = 1):
+    r"""
+    Conducts numerical differentiation
+
+    Parameters
+    ----------
+    c: array_like
+        array with scalar values, which serve as input for the defect function
+    defect_func: func
+        function which computes the defect with signature array_like(array_like, int)
+        where the second argument refers to the manipulated row
+    dc: float
+        base value for differentiation-interval
+    type: str
+        specifier for optimization of the process, currently supported arguments are
+        'full' and 'low_mem', for the allocation of an intermediated dense matrix
+        and sparse columns respectively. The option 'constrained' can be applied, if
+        the changes in defect are constrained to each pore, e.g. in the case of reaction
+    exclude: int|list[int]|None
+        component IDs for which the numerical differentiation shall not be conducted
+    axis: int
+        alternative to the `type` label for consistency with the mrm package
+    network: OpenPNM Network object
+        If provided, the sparsity structure of the network is exploited to speed up
+        the numerical differentiation. This option is only available for `type='full'`.
+        The network object is used to determine the sparsity
+        structure of the Jacobian.
+    stencil_size: int
+        number of additional adacent pores to include in the sparsity structure
+
+    Returns
+    -------
+    tuple of numerically determined Jacobian and defect
+
+    Notes
+    -----
+    To save memory during the computation, the matrix entries are stored per column
+    in a sparse array and later stacked to form the full sparse array.
+    The in between conversion leads to a slight overhead compared with the approach
+    to directly add the components into an existing array, but decreases memory
+    demand significantly, especially for large matrices (>5000 rows)
+    """
+    if len(c.shape) > 2:
+        raise ('Input array has invalid dimension, only 2D arrays are allowed!')
+    elif len(c.shape) < 2:
+        raise ('Input array has to have a second dimension, indicating the number of components')
+    type_l = type
+    if axis is not None:
+        if axis == 0 and type is None:
+            type_l = 'full'
+        elif axis == 1:
+            type_l = 'constrained'
+        else:
+            raise ValueError('axis value as to be either 0 or 1!')
+
+    exclude = [] if exclude is None else exclude
+    exclude = [exclude] if isinstance(exclude, int) else exclude
+    if not isinstance(exclude, list):
+        raise TypeError('the provided exclude list is not an integer or a list of integers')
+
+    num_param = len(signature(defect_func)._parameters)
+    if num_param == 0:
+        raise ValueError('The provided defect function does not take any arguments!')
+    elif num_param > 2:
+        raise ValueError('Number of arguments for defect function is larger than 2!')
+
+    dc_arr = _compute_dc(c.reshape((-1, 1)), dc)
+
+    if type_l == 'constrained':
+        J, G_0 = _apply_numerical_differentiation_locally_constrained(c=c,
+                                                                      defect_func=defect_func,
+                                                                      dc=dc_arr,
+                                                                      exclude=exclude)
+    elif type_l == 'full':
+        if network is not None:
+            J, G_0 = _apply_numerical_differentiation_exploit_sparsity(network=network,
+                                                                       c=c,
+                                                                       defect_func=defect_func,
+                                                                       dc=dc_arr,
+                                                                       stencil_size=1,
+                                                                       network=network)
+        else:
+            J, G_0 = _apply_numerical_differentiation_full(c=c,
+                                                           defect_func=defect_func,
+                                                           dc=dc_arr,
+                                                           exclude=exclude)
+    elif type_l == 'low_mem':
+        J, G_0 = _apply_numerical_differentiation_lowmem(c=c,
+                                                         defect_func=defect_func,
+                                                         dc=dc_arr,
+                                                         exclude=exclude)
+    else:
+        raise (f'Unknown type: {type}')
+    return scipy.sparse.csr_matrix(J), G_0.reshape((-1, 1))
 
 
 if __name__ == '__main__':  # pragma: no cover
